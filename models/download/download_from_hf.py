abc50be40214d1aeb70d317a4b764d4768f43ff8
yekta <ngyekta@gmail.com>
2023-02-10 16:30:25 +0300

    Download all models from HF

--- models/download/download_from_hf.py
+++ models/download/download_from_hf.py
@@ -1,14 +1,14 @@
-from models.stable_diffusion.constants import SD_MODELS, SD_MODEL_CACHE
+from models.stable_diffusion.constants import SD_MODELS_ALL, SD_MODEL_CACHE
 from diffusers import StableDiffusionPipeline
 import concurrent.futures
 
 
 def download_sd_model_from_hf(key):
-    model_id = SD_MODELS[key]["id"]
+    model_id = SD_MODELS_ALL[key]["id"]
     print(f"⏳ Downloading model: {model_id}")
     StableDiffusionPipeline.from_pretrained(
-        SD_MODELS[key]["id"],
-        torch_dtype=SD_MODELS[key]["torch_dtype"],
+        model_id,
+        torch_dtype=SD_MODELS_ALL[key]["torch_dtype"],
         cache_dir=SD_MODEL_CACHE,
     )
     print(f"✅ Downloaded model: {key}")
@@ -16,8 +16,16 @@ def download_sd_model_from_hf(key):
 
 
 def download_sd_models_concurrently_from_hf():
-    for key in SD_MODELS:
-        download_sd_model_from_hf(key)
+    with concurrent.futures.ThreadPoolExecutor(10) as executor:
+        # Start the download tasks
+        download_tasks = [
+            executor.submit(download_sd_model_from_hf, key) for key in SD_MODELS_ALL
+        ]
+        # Wait for all tasks to complete
+        results = [
+            task.result() for task in concurrent.futures.as_completed(download_tasks)
+        ]
+    executor.shutdown(wait=True)
 
 
 if __name__ == "__main__":
