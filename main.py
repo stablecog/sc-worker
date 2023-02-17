736ee8bd2ffc0b84ce9c1d190904947129524e00
yekta <ngyekta@gmail.com>
2023-02-17 09:15:01 +0000

    Use constants

--- main.py
+++ main.py
@@ -11,32 +11,27 @@ from dotenv import load_dotenv
 
 from predict.setup import setup
 from rdqueue.worker import start_redis_queue_worker
+from upload.constants import S3_ACCESS_KEY_ID, S3_BUCKET_NAME_MODELS, S3_BUCKET_NAME_UPLOAD, S3_ENDPOINT_URL, S3_REGION, S3_SECRET_ACCESS_KEY
 from upload.worker import start_upload_worker
 
 if __name__ == "__main__":
     load_dotenv()
 
-    s3Region = os.environ.get("S3_REGION")
-    s3EndpointUrl = os.environ.get("S3_ENDPOINT_URL")
-    s3BucketNameUpload = os.environ.get("S3_BUCKET_NAME_UPLOAD")
-    s3BucketNameModels = os.environ.get("S3_BUCKET_NAME_MODELS")
-    s3AccessKey = os.environ.get("AWS_ACCESS_KEY_ID")
-    s3SecretKey = os.environ.get("AWS_SECRET_ACCESS_KEY")
     redisUrl = os.environ.get("REDIS_URL")
     redisInputQueue = os.environ.get("REDIS_INPUT_QUEUE")
 
     # Configure S3 client
     s3: ServiceResource = boto3.resource(
         "s3",
-        region_name=s3Region,
-        endpoint_url=s3EndpointUrl,
-        aws_access_key_id=s3AccessKey,
-        aws_secret_access_key=s3SecretKey,
+        region_name=S3_REGION,
+        endpoint_url=S3_ENDPOINT_URL,
+        aws_access_key_id=S3_ACCESS_KEY_ID,
+        aws_secret_access_key=S3_SECRET_ACCESS_KEY,
         config=Config(retries={"max_attempts": 3, "mode": "standard"}),
     )
 
     # Setup predictor
-    model_pack = setup(s3, s3BucketNameModels)
+    model_pack = setup(s3, S3_BUCKET_NAME_MODELS)
 
     # Setup redis
     redis = redis.from_url(redisUrl)
@@ -50,7 +45,7 @@ if __name__ == "__main__":
             redis,
             input_queue=redisInputQueue,
             s3_client=s3,
-            s3_bucket=s3BucketNameUpload,
+            s3_bucket=S3_BUCKET_NAME_UPLOAD,
             upload_queue=upload_queue,
             model_pack=model_pack,
         )
@@ -59,7 +54,7 @@ if __name__ == "__main__":
     # Create upload thread
     upload_thread = Thread(
         target=lambda: start_upload_worker(
-            q=upload_queue, s3=s3, s3_bucket=s3BucketNameUpload, redis=redis
+            q=upload_queue, s3=s3, s3_bucket=S3_BUCKET_NAME_UPLOAD, redis=redis
         )
     )
 
---
+++ upload/constants.py
@@ -0,0 +1,8 @@
+import os
+
+S3_REGION = os.environ.get("S3_REGION")
+S3_ENDPOINT_URL = os.environ.get("S3_ENDPOINT_URL")
+S3_BUCKET_NAME_UPLOAD = os.environ.get("S3_BUCKET_NAME_UPLOAD")
+S3_BUCKET_NAME_MODELS = os.environ.get("S3_BUCKET_NAME_MODELS")
+S3_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+S3_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
