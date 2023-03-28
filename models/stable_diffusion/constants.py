fdeb455d5d28a02b95ffb3ec1d703a672c8c6212
yekta <ngyekta@gmail.com>
2023-03-29 00:59:19 +0300

    Model updates

--- models/stable_diffusion/constants.py
+++ models/stable_diffusion/constants.py
@@ -59,8 +59,20 @@ SD_MODELS_ALL = {
         "torch_dtype": torch.float16,
         "branch": "fp16",
     },
+    "Waifu Diffusion": {
+        "id": "hakurei/waifu-diffusion",
+        "prompt_prefix": "masterpiece, best quality, high quality",
+        "negative_prompt_prefix": "worst quality, low quality, deleted, nsfw, blurry",
+        "torch_dtype": torch.float16,
+        "branch": "fp16",
+    },
     "22h Diffusion v0.1": {
-        "id": "22h/vintedois-diffusion-v0-1",
+        "id": "22h/vintedois-diffusion-v0-2",
+        "prompt_prefix": "estilovintedois",
+        "torch_dtype": torch.float16,
+    },
+    "22h Diffusion": {
+        "id": "22h/vintedois-diffusion-v0-2",
         "prompt_prefix": "estilovintedois",
         "torch_dtype": torch.float16,
     },
