42749dceead1813aa5261ab4fd67127c8e2a4ff8
yekta <ngyekta@gmail.com>
2023-03-31 20:19:46 +0000

    Remove clip_prompt_embed

--- predict/classes.py
+++ predict/classes.py
@@ -7,16 +7,12 @@ class PredictOutput:
         pil_image: Image.Image,
         target_extension: str,
         target_quality: int,
-        clip_image_embed: list[float],
-        clip_prompt_embed: list[float],
         open_clip_image_embed: list[float],
         open_clip_prompt_embed: list[float],
     ):
         self.pil_image = pil_image
         self.target_extension = target_extension
         self.target_quality = target_quality
-        self.clip_image_embed = clip_image_embed
-        self.clip_prompt_embed = clip_prompt_embed
         self.open_clip_image_embed = open_clip_image_embed
         self.open_clip_prompt_embed = open_clip_prompt_embed
 
