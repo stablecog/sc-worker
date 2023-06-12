48be5d69a0e1b9dc015225fb8f117467faf35a64
yekta <ngyekta@gmail.com>
2023-06-12 04:07:26 +0300

    Denoise

--- models/audio_denoiser/denoise_audio.py
+++ models/audio_denoiser/denoise_audio.py
@@ -5,11 +5,18 @@ from denoiser.dsp import convert_audio
 from io import BytesIO
 import torchaudio
 
+from shared.helpers import numpy_to_wav_bytes
+
 
 def denoise_audio(audio: BytesIO, model: Any) -> np.ndarray:
     wav, sr = torchaudio.load(audio)
-    wav = convert_audio(wav.cuda(), sr, model.sample_rate, model.chin)
-    print(wav)
+    wav = convert_audio(
+        wav=wav.cuda(),
+        from_samplerate=sr,
+        to_samplerate=model.sample_rate,
+        channels=model.chin,
+    )
     with torch.no_grad():
         denoised_audio = model(wav[None])[0]
+    denoised_audio = numpy_to_wav_bytes(denoise_audio, model.sample_rate)
     return denoised_audio
