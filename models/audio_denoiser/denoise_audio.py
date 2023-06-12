97c46aab8e46b3c6995f38db44f80301c212ddad
yekta <ngyekta@gmail.com>
2023-06-12 04:51:08 +0300

    Tensor

--- models/audio_denoiser/denoise_audio.py
+++ models/audio_denoiser/denoise_audio.py
@@ -12,11 +12,11 @@ def denoise_audio(
     audio: np.array, sample_rate: int, model: Any
 ) -> Tuple[np.ndarray, int]:
     s = time.time()
-    wavfile.write("temp.wav", sample_rate, audio)
-    wav, sr = torchaudio.load("temp.wav")
+    tensor = torch.from_numpy(audio).cuda()
+    tensor = tensor.view(1, -1)
     wav = convert_audio(
-        wav=wav.cuda(),
-        from_samplerate=sr,
+        wav=tensor,
+        from_samplerate=sample_rate,
         to_samplerate=model.sample_rate,
         channels=model.chin,
     )
@@ -24,5 +24,5 @@ def denoise_audio(
         denoised_audio = model(wav[None])[0]
     arr = denoised_audio.data.cpu().numpy()
     e = time.time()
-    print(f"🔊 Denoised audio in: {round(e - s, 2)} sec.🔊")
+    print(f"🔊 Denoised audio in: {round(e - s, 2)} sec🔊")
     return arr, model.sample_rate
