a13e5fa4de4f97942b67ebff0a1ead3a0245b7f8
yekta <ngyekta@gmail.com>
2023-06-12 03:31:28 +0300

    Print

--- models/audio_denoiser/denoise_audio.py
+++ models/audio_denoiser/denoise_audio.py
@@ -7,5 +7,7 @@ from denoiser.dsp import convert_audio
 def denoise_audio(audio: np.ndarray, sample_rate: int, model: Any) -> np.ndarray:
     wav = convert_audio(audio, sample_rate, model.sample_rate, model.chin)
     with torch.no_grad():
+        res = model(wav)
+        print(res.shape)
         denoised_audio = model(wav[None])[0]
     return denoised_audio
