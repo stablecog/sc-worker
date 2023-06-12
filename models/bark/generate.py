29c2326d8cb2dbf287528706a8612ce3d0a5e781
yekta <ngyekta@gmail.com>
2023-06-12 04:35:12 +0300

    Remove denoise

--- models/bark/generate.py
+++ models/bark/generate.py
@@ -63,13 +63,13 @@ def generate_voiceover(
     np_array = np.concatenate(pieces)
     sample_rate = SAMPLE_RATE
 
-    if should_denoise:
+    """ if should_denoise:
         wav, sr = denoise_audio(
             audio=np_array,
             sample_rate=SAMPLE_RATE,
             model=denoiser_model,
         )
-        sample_rate = sr
+        sample_rate = sr """
 
     audio_duration = len(np_array) / sample_rate
     wav = numpy_to_wav_bytes(np_array, sample_rate)
