563ec9448d3e269f1ae87f0a0168579d01e01600
yekta <ngyekta@gmail.com>
2023-06-12 04:25:12 +0300

    Try transposing

--- models/audio_denoiser/denoise_audio.py
+++ models/audio_denoiser/denoise_audio.py
@@ -2,13 +2,16 @@ import numpy as np
 from typing import Any
 import torch
 from denoiser.dsp import convert_audio
+from scipy.io import wavfile
+import torchaudio
 
 
 def denoise_audio(audio: np.array, sample_rate: int, model: Any) -> np.ndarray:
-    print(f"Shape is: {audio.shape}")
-    tensor = torch.from_numpy(audio).cuda()
+    reshaped_data = audio.reshape(-1, 2)
+    transposed_data = reshaped_data.T
+    tensor_data = torch.from_numpy(transposed_data).cuda()
     wav = convert_audio(
-        wav=tensor,
+        wav=tensor_data,
         from_samplerate=sample_rate,
         to_samplerate=model.sample_rate,
         channels=model.chin,
