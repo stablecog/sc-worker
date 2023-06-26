43bd745ef7f155bd7e0d9630dcfd6a6e77750946
yekta <ngyekta@gmail.com>
2023-06-26 20:54:35 +0300

    Remove scheduler

--- models/kandinsky/constants.py
+++ models/kandinsky/constants.py
@@ -1,13 +1,4 @@
-from diffusers import DDPMScheduler, DDIMScheduler
-
-KANDIKSKY_SCHEDULERS = {
-    "P_SAMPLER": DDPMScheduler.from_pretrained(
-        "kandinsky-community/kandinsky-2-1", subfolder="ddpm_scheduler"
-    ),
-    "DDIM": DDIMScheduler.from_pretrained(
-        "kandinsky-community/kandinsky-2-1", subfolder="ddim_scheduler"
-    ),
-}
+KANDIKSKY_SCHEDULERS = {"P_SAMPLER": "p", "DDIM": "d"}
 
 KANDINSKY_SCHEDULER_CHOICES = [*KANDIKSKY_SCHEDULERS.keys()]
 
