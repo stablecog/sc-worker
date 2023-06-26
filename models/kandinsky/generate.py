94887233259a88646b477903ef7c0546f8e13522
yekta <ngyekta@gmail.com>
2023-06-26 20:40:46 +0300

    SD style nsfw check on Kandinsky

--- models/kandinsky/generate.py
+++ models/kandinsky/generate.py
@@ -56,7 +56,7 @@ def generate_with_kandinsky(
         "generator": generator,
     }
 
-    output_images = None
+    output = None
     if init_image_url is not None:
         start_i = time.time()
         init_image = download_image(init_image_url)
@@ -65,38 +65,29 @@ def generate_with_kandinsky(
         print(
             f"-- Downloaded and cropped init image in: {round((end_i - start_i) * 1000)} ms"
         )
-        output_images = pipe["img2img"](
+        output = pipe["img2img"](
             image=init_image,
             strength=prompt_strength,
             **args,
         )
     else:
-        output_images = pipe["text2img"](
+        output = pipe["text2img"](
             **args,
-        ).images
+        )
 
-    output_images_nsfw_results = []
-    with autocast():
-        for image in output_images:
-            has_nsfw_concepts = False
-            result = None
-            if safety_checker is not None:
-                safety_checker_input = safety_checker["feature_extractor"](
-                    images=image, return_tensors="pt"
-                ).to("cuda")
-                result, has_nsfw_concepts = safety_checker["checker"].forward(
-                    clip_input=safety_checker_input.pixel_values, images=image
-                )
-            res = {
-                "result": result,
-                "has_nsfw_concepts": has_nsfw_concepts,
-            }
-            output_images_nsfw_results.append(res)
+    output_images = []
     nsfw_count = 0
-    filtered_output_images = []
-    for i, res in enumerate(output_images_nsfw_results):
-        if res["has_nsfw_concepts"]:
-            nsfw_count += 1
-        else:
-            filtered_output_images.append(output_images[i])
-    return filtered_output_images, nsfw_count
+
+    if output.nsfw_content_detected is not None:
+        for i, nsfw_flag in enumerate(output.nsfw_content_detected):
+            if nsfw_flag:
+                nsfw_count += 1
+            else:
+                output_images.append(output.images[i])
+    else:
+        output_images = output.images
+
+    if nsfw_count > 0:
+        print(f"NSFW content detected in {nsfw_count}/{num_outputs} of the outputs.")
+
+    return output_images, nsfw_count
