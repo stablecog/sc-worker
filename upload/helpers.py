f016bb7e15939fd941f2927eed9f992eeaec8cc8
yekta <ngyekta@gmail.com>
2023-06-29 16:32:13 +0300

    Quarter

--- upload/helpers.py
+++ upload/helpers.py
@@ -27,7 +27,7 @@ def get_waveform_image_url(
     return f"https://og.stablecog.com/api/voiceover/waveform.png?speaker={encoded_speaker}&prompt={encoded_prompt}=&audio_array={audio_array_string}"
 
 
-def audio_array_from_wav(wav_bytes: BytesIO, count: int = 50):
+def audio_array_from_wav(wav_bytes: BytesIO, count: int):
     # Read the WAV data
     wav_bytes.seek(0)
     sample_rate, audio_data = wav_read(wav_bytes)
@@ -39,18 +39,20 @@ def audio_array_from_wav(wav_bytes: BytesIO, count: int = 50):
     # Convert to floating point
     audio_data = audio_data.astype(np.float32)
 
-    # Pick a specific point (middle) in each window
+    # Pick two points in each window and average them
     window_size = len(audio_data) // count
     audio_data = audio_data[: window_size * count]  # Discard end samples if necessary
     audio_data = audio_data.reshape(-1, window_size)
-    point_values = audio_data[:, window_size // 2]
+    point_values1 = audio_data[:, window_size // 4]
+    point_values2 = audio_data[:, window_size * 3 // 4]
+    avg_values = (point_values1 + point_values2) / 2
 
-    # Convert point values to dB
-    point_db = 20 * np.log10(np.maximum(1e-5, np.abs(point_values)))
+    # Convert average values to dB
+    avg_db = 20 * np.log10(np.maximum(1e-5, np.abs(avg_values)))
 
     # Normalize dB values to [0, 1] range
-    point_db_min, point_db_max = point_db.min(), point_db.max()
-    audio_data = (point_db - point_db_min) / (point_db_max - point_db_min)
+    avg_db_min, avg_db_max = avg_db.min(), avg_db.max()
+    audio_data = (avg_db - avg_db_min) / (avg_db_max - avg_db_min)
 
     audio_data_list = [float(x) for x in audio_data]
     return audio_data_list
