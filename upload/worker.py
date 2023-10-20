b3ae33a6a116031555f4302e2c6b6a4a6570c8d1
Brandon Berhent <bbedward@gmail.com>
2023-10-20 09:50:19 -0400

    Logging cleanup

--- upload/worker.py
+++ upload/worker.py
@@ -23,19 +23,19 @@ def start_upload_worker(
     shutdown_event: Event,
 ):
     """Starts a loop to read from the queue and upload files to S3, send responses to webhook"""
-    logging.info("Starting upload thread...\n")
+    logging.info("Starting upload thread...")
     while not shutdown_event.is_set() or not q.empty():
         try:
-            logging.info(f"-- Upload: Waiting for queue --\n")
+            # logging.info(f"-- Upload: Waiting for queue --\n")
             uploadMsg: List[Dict[str, Any]] = q.get(timeout=1)
-            logging.info(f"-- Upload: Got from queue --\n")
+            # logging.info(f"-- Upload: Got from queue --\n")
             if "upload_output" in uploadMsg:
                 predict_result: PredictResultForImage | PredictResultForVoiceover = (
                     uploadMsg["upload_output"]
                 )
                 if len(predict_result.outputs) > 0:
                     logging.info(
-                        f"-- Upload: Uploading {len(predict_result.outputs)} files --\n"
+                        f"-- Upload: Uploading {len(predict_result.outputs)} files --"
                     )
                     try:
                         if worker_type == "voiceover":
@@ -64,16 +64,16 @@ def start_upload_worker(
                         logging.error(f"Error uploading files {tb}\n")
                         uploadMsg["status"] = Status.FAILED
                         uploadMsg["error"] = str(e)
-                    logging.info(f"-- Upload: Finished uploading files --\n")
+                    logging.info(f"-- Upload: Finished uploading files --")
 
             if "upload_output" in uploadMsg:
-                logging.info(f"-- Upload: Deleting upload_output from message --\n")
+                logging.info(f"-- Upload: Deleting upload_output from message --")
                 del uploadMsg["upload_output"]
             if "upload_prefix" in uploadMsg:
-                logging.info(f"-- Upload: Deleting upload_prefix from message --\n")
+                logging.info(f"-- Upload: Deleting upload_prefix from message --")
                 del uploadMsg["upload_prefix"]
 
-            logging.info(f"-- Upload: Publishing to WEBHOOK --\n")
+            logging.info(f"-- Upload: Publishing to WEBHOOK --")
             post_webhook(uploadMsg["webhook_url"], uploadMsg)
         except queue.Empty:
             continue
