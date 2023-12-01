f0e21ecedd33ba7088bb385b1d7ec18b36b4b484
yekta <ngyekta@gmail.com>
2023-12-01 09:51:06 +0300

    Revert "Quick fix"
    This reverts commit a79452abd35d440f5c66aec6ea83adb25a342bbf.

--- predict/image/setup.py
+++ predict/image/setup.py
@@ -223,9 +223,10 @@ def setup() -> ModelsPack:
                 add_watermarker=False,
             )
             text2img = text2img.to(DEVICE)
+            img2img = AutoPipelineForText2Image(**text2img.components)
             pipe = SDPipe(
                 text2img=text2img,
-                img2img=text2img,
+                img2img=img2img,
                 inpaint=None,
                 refiner=None,
                 refiner_inpaint=None,
