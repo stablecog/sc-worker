a5b941f882ac633d1b6da74edc67e896d650c994
yekta <ngyekta@gmail.com>
2024-06-27 07:22:57 +0300

    Log memory

--- shared/vram.py
+++ shared/vram.py
@@ -1,4 +1,6 @@
 import torch
+from shared.logger import logger
+from tabulate import tabulate
 
 
 def get_device_vram_gb(device_id=0):
@@ -6,7 +8,12 @@ def get_device_vram_gb(device_id=0):
         device_properties = torch.cuda.get_device_properties(device_id)
         total_memory = device_properties.total_memory
         total_memory_gb = total_memory / (1024**3)
-
+        total_memory_gb_str = f"{total_memory_gb:.1f} GB"
+        logger.info(
+            tabulate(
+                [["Total GPU Memory", total_memory_gb_str]], tablefmt="double_grid"
+            )
+        )
         return total_memory_gb
     else:
         return 0
