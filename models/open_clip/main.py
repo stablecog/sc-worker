6c25385683914280b728d2a73412e19a2ec696ec
yekta <ngyekta@gmail.com>
2024-07-25 02:51:19 +0300

    Clip log improvements

--- models/open_clip/main.py
+++ models/open_clip/main.py
@@ -61,13 +61,13 @@ def clip_preprocessor(images: List[Image.Image], return_tensors="pt"):
 @time_it
 def open_clip_get_embeds_of_images(images: List[Image.Image], model, processor):
     with torch.no_grad():
-        with time_code_block(prefix=f"// Preprocessed {len(images)} image(s)"):
+        with time_code_block(prefix=f"📎 Preprocessed {len(images)} image(s)"):
             inputs = clip_preprocessor(images=images, return_tensors="pt")
         inputs = inputs.to(DEVICE_CUDA)
-        with time_code_block(prefix=f"// Embedded {len(images)} image(s)"):
+        with time_code_block(prefix=f"📎 Embedded {len(images)} image(s)"):
             image_embeddings = model.get_image_features(pixel_values=inputs)
         with time_code_block(
-            prefix=f"// Moved {len(images)} embedding(s) to CPU as list"
+            prefix=f"📎 Moved {len(images)} embedding(s) to CPU as list"
         ):
             image_embeddings = image_embeddings.cpu().numpy().tolist()
         return image_embeddings
@@ -85,8 +85,8 @@ def open_clip_get_embeds_of_texts(texts: str, model, tokenizer):
                 max_length=OPEN_CLIP_TOKEN_LENGTH_MAX,
             )
         inputs = inputs.to(DEVICE_CUDA)
-        with time_code_block(prefix=f"Embedded {len(texts)} text(s)"):
+        with time_code_block(prefix=f"📎 Embedded {len(texts)} text(s)"):
             text_embeddings = model.get_text_features(**inputs)
-        with time_code_block(prefix=f"Moved {len(texts)} embeddings(s) to CPU"):
+        with time_code_block(prefix=f"📎 Moved {len(texts)} embeddings(s) to CPU"):
             text_embeddings = text_embeddings.cpu().numpy().tolist()
         return text_embeddings
