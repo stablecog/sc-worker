212ebbb9d77c00a33240c0a72aae2e686b89dd1e
yekta <ngyekta@gmail.com>
2024-08-17 00:58:17 +0300

    Indicate put to queue success in logs

--- rabbitmq_consumer/worker.py
+++ rabbitmq_consumer/worker.py
@@ -110,9 +110,9 @@ def create_amqp_callback(
                     response["upload_output"],
                     (PredictResultForImage),
                 ):
-                    logging.info(f"^^ Putting to queue")
+                    logging.info(f"^^ 🟡 Putting to queue")
                     upload_queue.put(response)
-                    logging.info(f"^^ Put to queue")
+                    logging.info(f"^^ 🟢 Put to queue")
                 elif response_event in events_filter:
                     status_code = post_webhook(webhook_url, response)
                     logging.info(f"-- Webhook: {status_code}")
