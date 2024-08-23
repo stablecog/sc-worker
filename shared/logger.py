5f2550e1a7820546cf80100d235b328da4af3b1f
yekta <ngyekta@gmail.com>
2024-08-23 05:18:18 +0300

    Try again

--- shared/logger.py
+++ shared/logger.py
@@ -1,38 +1,29 @@
 import logging
-import logging.handlers
 import logging_loki
-from multiprocessing import Queue
 import os
-import uuid
-import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-class TerminalCapture:
-    def __init__(self, original_stream, loki_handler):
-        self.original_stream = original_stream
-        self.loki_handler = loki_handler
+class LokiHandler(logging_loki.LokiHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
-    def write(self, message):
-        self.original_stream.write(message)
-        self.original_stream.flush()
-        if message.strip():
-            # Send raw message to Loki without formatting
-            record = logging.LogRecord(
-                name="terminal",
-                level=logging.INFO,
-                pathname="",
-                lineno=0,
-                msg=message.strip(),
-                args=(),
-                exc_info=None,
+    def emit(self, record):
+        if record.name == "root" and record.levelno == logging.INFO:
+            # For print statements, send the raw message
+            self.formatter = None
+        else:
+            # For logger calls, use the formatted message
+            self.formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
             )
-            self.loki_handler.emit(record)
 
-    def flush(self):
-        self.original_stream.flush()
+        super().emit(record)
 
 
 def setup_logger():
@@ -40,45 +31,49 @@ def setup_logger():
     loki_url = os.getenv("LOKI_URL")
     loki_username = os.getenv("LOKI_USERNAME")
     loki_password = os.getenv("LOKI_PASSWORD")
-    worker_name = os.getenv("WORKER_NAME", str(uuid.uuid4()))
+    worker_name = os.getenv("WORKER_NAME", "default_worker")
 
     # Validate environment variables
-    if not loki_url:
-        raise ValueError("LOKI_URL environment variable is not set")
-    if not loki_username:
-        raise ValueError("LOKI_USERNAME environment variable is not set")
-    if not loki_password:
-        raise ValueError("LOKI_PASSWORD environment variable is not set")
+    if not all([loki_url, loki_username, loki_password]):
+        raise ValueError("Loki environment variables are not set properly")
 
     # Set up the Loki handler
-    loki_handler = logging_loki.LokiHandler(
+    loki_handler = LokiHandler(
         url=f"{loki_url}/loki/api/v1/push",
         tags={"worker_name": worker_name, "application": "sc-worker"},
         auth=(loki_username, loki_password),
         version="1",
     )
 
-    # Set up formatter for logger calls
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    # Set up the console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     )
-    loki_handler.setFormatter(formatter)
 
     # Set up the root logger
     root_logger = logging.getLogger()
-    if root_logger.hasHandlers():
-        root_logger.handlers.clear()
-    root_logger.addHandler(loki_handler)
     root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(loki_handler)
+    root_logger.addHandler(console_handler)
 
-    # Capture terminal output
-    sys.stdout = TerminalCapture(sys.stdout, loki_handler)
-    sys.stderr = TerminalCapture(sys.stderr, loki_handler)
+    # Capture print statements
+    print_logger = logging.getLogger("print")
+    print_logger.setLevel(logging.INFO)
+
+    def print_override(*args, sep=" ", end="\n", file=None):
+        message = sep.join(map(str, args)) + end
+        print_logger.info(message)
+        if file:
+            print(*args, sep=sep, end=end, file=file)
+        else:
+            __builtins__["print"](*args, sep=sep, end=end)
+
+    __builtins__["print"] = print_override
 
     return root_logger
 
 
-# Function to restore original stdout and stderr
-def restore_streams():
-    sys.stdout = sys.__stdout__
-    sys.stderr = sys.__stderr__
+# To be called at the end of your application
+def teardown_logger():
+    __builtins__["print"] = __builtins__["__print__"]
