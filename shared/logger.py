93d759500a641ef724034d390de035868527d9d9
yekta <ngyekta@gmail.com>
2024-08-23 05:38:08 +0300

    Back to normal logger

--- shared/logger.py
+++ shared/logger.py
@@ -10,44 +10,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class PrintCapturer:
-    def __init__(self, queue, original_stream):
-        self.queue = queue
-        self.original_stream = original_stream
-
-    def write(self, message):
-        if message.strip():
-            # Send the raw message to the queue
-            record = logging.LogRecord(
-                name="print",
-                level=logging.INFO,
-                pathname="",
-                lineno=0,
-                msg=message,
-                args=(),
-                exc_info=None,
-            )
-            self.queue.put_nowait(record)
-        # Always write to the original stream
-        self.original_stream.write(message)
-
-    def flush(self):
-        self.original_stream.flush()
-
-
-class LokiHandler(logging_loki.LokiHandler):
-    def emit(self, record):
-        if record.name == "print":
-            # For print statements, send the raw message
-            self.send_message(record.msg)
-        else:
-            # For other log records, use the standard formatting
-            super().emit(record)
-
-    def send_message(self, message):
-        self._write_message(message)
-
-
 def setup_logger():
     # Fetch environment variables
     loki_url = os.getenv("LOKI_URL")
@@ -63,11 +25,12 @@ def setup_logger():
     if not loki_password:
         raise ValueError("LOKI_PASSWORD environment variable is not set")
 
-    # Set up the logging queue
+    # Set up the logging queue and handler
     queue = Queue(-1)
+    queue_handler = logging.handlers.QueueHandler(queue)
 
-    # Set up the custom Loki handler
-    handler_loki = LokiHandler(
+    # Set up the Loki handler
+    handler_loki = logging_loki.LokiHandler(
         url=f"{loki_url}/loki/api/v1/push",
         tags={"worker_name": worker_name, "application": "sc-worker"},
         auth=(loki_username, loki_password),
@@ -93,11 +56,7 @@ def setup_logger():
     # Clear existing handlers to avoid double logging
     if root_logger.hasHandlers():
         root_logger.handlers.clear()
-    root_logger.addHandler(logging.handlers.QueueHandler(queue))
+    root_logger.addHandler(queue_handler)
     root_logger.setLevel(logging.INFO)
-
-    # Capture stdout and stderr
-    sys.stdout = PrintCapturer(queue, sys.stdout)
-    sys.stderr = PrintCapturer(queue, sys.stderr)
 
     return listener
