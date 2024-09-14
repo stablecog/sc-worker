c3434c2cd1cf121b836d9083af4ce2f2cb0bb6cb
yekta <ngyekta@gmail.com>
2024-09-14 13:51:32 +0300

    Load model files in parallel

--- models/flux1/constants.py
+++ models/flux1/constants.py
@@ -7,7 +7,5 @@ FLUX1_REPO = "black-forest-labs/FLUX.1-schnell"
 FLUX1_KEEP_IN_CPU_WHEN_IDLE = device_vram_gb < 40
 FLUX1_DTYPE = torch.bfloat16
 
-FLUX1_FP8_TRANSFORMER_FILE = (
-    "https://huggingface.co/Kijai/flux-fp8/blob/main/flux1-schnell-fp8.safetensors"
-)
+FLUX1_FP8_TRANSFORMER_FILE = "https://huggingface.co/Kijai/flux-fp8/blob/main/flux1-schnell-fp8-e4m3fn.safetensors"
 FLUX1_LOAD = MODELS_FROM_ENV == "all" or FLUX1_MODEL_NAME in MODELS_FROM_ENV_LIST
--- predict/image/setup.py
+++ predict/image/setup.py
@@ -1,7 +1,8 @@
 import os
 import time
-
 import torch
+import logging
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from diffusers import (
     KandinskyV22Pipeline,
     KandinskyV22PriorPipeline,
@@ -16,10 +17,10 @@ from diffusers import (
 )
 from transformers import T5EncoderModel
 from huggingface_hub import login
-
 from models.constants import DEVICE_CPU, DEVICE_CUDA
 from models.flux1.constants import (
     FLUX1_DTYPE,
+    FLUX1_FP8_TRANSFORMER_FILE,
     FLUX1_KEEP_IN_CPU_WHEN_IDLE,
     FLUX1_MODEL_NAME,
     FLUX1_REPO,
@@ -45,21 +46,209 @@ from predict.image.classes import (
     Upscaler,
 )
 from shared.constants import WORKER_VERSION, TabulateLevels
-import logging
 from tabulate import tabulate
 from aura_sr import AuraSR
-
-
 from shared.helpers import time_log
 
 
-def auto_move_to_device(dict, key, pipe, description):
-    if dict[key].get("keep_in_cpu_when_idle"):
+def auto_move_to_device(model_dict, key, pipe, description):
+    if model_dict[key].get("keep_in_cpu_when_idle"):
         logging.info(f"🐌 Keep in {DEVICE_CPU} when idle: {description}")
         return pipe.to(DEVICE_CPU, silence_dtype_warnings=True)
     else:
         logging.info(f"🚀 Keep in {DEVICE_CUDA}: {description}")
         return pipe.to(DEVICE_CUDA)
+
+
+def load_flux1_model():
+    f1_s = time.time()
+    with time_log(f"Load {FLUX1_MODEL_NAME} transformer"):
+        f1_transformer = FluxTransformer2DModel.from_single_file(
+            FLUX1_FP8_TRANSFORMER_FILE,
+            torch_dtype=FLUX1_DTYPE,
+        )
+    with time_log(f"Quantize {FLUX1_MODEL_NAME} transformer"):
+        quantize(f1_transformer, weights=qfloat8)
+
+    with time_log(f"Freeze {FLUX1_MODEL_NAME} transformer"):
+        freeze(f1_transformer)
+
+    with time_log(f"Load {FLUX1_MODEL_NAME} text_encoder_2"):
+        f1_text_encoder_2 = T5EncoderModel.from_pretrained(
+            FLUX1_REPO, subfolder="text_encoder_2", torch_dtype=FLUX1_DTYPE
+        )
+    with time_log(f"Quantize {FLUX1_MODEL_NAME} text_encoder_2"):
+        quantize(f1_text_encoder_2, weights=qfloat8)
+
+    with time_log(f"Freeze {FLUX1_MODEL_NAME} text_encoder_2"):
+        freeze(f1_text_encoder_2)
+
+    f1_pipe = FluxPipeline.from_pretrained(
+        FLUX1_REPO, transformer=None, text_encoder_2=None, torch_dtype=FLUX1_DTYPE
+    )
+    f1_pipe.transformer = f1_transformer
+    f1_pipe.text_encoder_2 = f1_text_encoder_2
+    if FLUX1_KEEP_IN_CPU_WHEN_IDLE:
+        logging.info(f"🐌 Keep in {DEVICE_CPU} when idle: {FLUX1_MODEL_NAME}")
+    else:
+        f1_pipe = f1_pipe.to(DEVICE_CUDA)
+        logging.info(f"🚀 Keep in {DEVICE_CUDA}: {FLUX1_MODEL_NAME}")
+    flux1 = Flux1PipeSet(
+        text2img=f1_pipe,
+    )
+    f1_e = time.time()
+    logging.info(
+        f"✅ Loaded {FLUX1_MODEL_NAME} | Duration: {round(f1_e - f1_s, 1)} sec."
+    )
+    return flux1
+
+
+def load_sd_model(key):
+    s = time.time()
+    logging.info(f"🟡 Loading SD model: {key}")
+
+    base_model = SD_MODELS[key].get("base_model", None)
+
+    if base_model == "SDXL":
+        args = {
+            "pretrained_model_name_or_path": SD_MODELS[key]["id"],
+            "torch_dtype": SD_MODELS[key]["torch_dtype"],
+            "cache_dir": SD_MODEL_CACHE,
+            "use_safetensors": True,
+            "add_watermarker": False,
+        }
+        if "variant" in SD_MODELS[key]:
+            args["variant"] = SD_MODELS[key]["variant"]
+        text2img = StableDiffusionXLPipeline.from_pretrained(**args)
+
+        if "default_lora" in SD_MODELS[key]:
+            lora = SD_MODELS[key]["default_lora"]
+            text2img.load_lora_weights(
+                SD_MODELS[key]["id"],
+                weight_name=lora,
+            )
+            logging.info(f"✅ Loaded LoRA weights: {lora}")
+
+        refiner = None
+        if SD_MODELS[key].get("refiner_id") is not None:
+            refiner_args = {
+                "pretrained_model_name_or_path": SD_MODELS[key]["refiner_id"],
+                "torch_dtype": SD_MODELS[key]["torch_dtype"],
+                "cache_dir": SD_MODEL_CACHE,
+                "use_safetensors": True,
+                "add_watermarker": False,
+            }
+            if "variant" in SD_MODELS[key]:
+                refiner_args["variant"] = SD_MODELS[key]["variant"]
+            refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained(**refiner_args)
+            refiner = auto_move_to_device(SD_MODELS, key, refiner, f"{key} refiner")
+
+        text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
+        img2img = StableDiffusionXLImg2ImgPipeline(**text2img.components)
+
+        inpaint = None
+        pipe = SDPipeSet(
+            text2img=text2img,
+            img2img=img2img,
+            inpaint=inpaint,
+            refiner=refiner,
+        )
+    elif base_model == "Stable Diffusion 3":
+        args = {
+            "torch_dtype": SD_MODELS[key]["torch_dtype"],
+            "cache_dir": SD_MODEL_CACHE,
+            "safety_checker": None,
+        }
+        if "from_single_file_url" in SD_MODELS[key]:
+            args["pretrained_model_link_or_path"] = SD_MODELS[key][
+                "from_single_file_url"
+            ]
+        else:
+            args["pretrained_model_name_or_path"] = SD_MODELS[key]["id"]
+            args["text_encoder_3"] = None
+            args["tokenizer_3"] = None
+
+        if "variant" in SD_MODELS[key]:
+            args["variant"] = SD_MODELS[key]["variant"]
+
+        if "from_single_file_url" in SD_MODELS[key]:
+            url = SD_MODELS[key]["from_single_file_url"]
+            logging.info(f"🟡 Loading SD model: {key} from single file: {url}")
+            text2img = StableDiffusion3Pipeline.from_single_file(**args)
+        else:
+            text2img = StableDiffusion3Pipeline.from_pretrained(**args)
+        text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
+        img2img = StableDiffusion3Img2ImgPipeline(**text2img.components)
+        pipe = SDPipeSet(
+            text2img=text2img,
+            img2img=img2img,
+            inpaint=None,
+            refiner=None,
+        )
+    else:
+        args = {
+            "pretrained_model_name_or_path": SD_MODELS[key]["id"],
+            "torch_dtype": SD_MODELS[key]["torch_dtype"],
+            "cache_dir": SD_MODEL_CACHE,
+            "safety_checker": None,
+        }
+        if "variant" in SD_MODELS[key]:
+            args["variant"] = SD_MODELS[key]["variant"]
+        text2img = StableDiffusionPipeline.from_pretrained(**args)
+        text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
+        img2img = StableDiffusionImg2ImgPipeline(**text2img.components)
+
+        pipe = SDPipeSet(
+            text2img=text2img,
+            img2img=img2img,
+            inpaint=None,
+            refiner=None,
+        )
+
+    logging.info(
+        f"✅ Loaded SD model: {key} | Duration: {round(time.time() - s, 1)} seconds"
+    )
+    return pipe
+
+
+def load_kandinsky_2_2():
+    s = time.time()
+    logging.info("🟡 Loading Kandinsky 2.2")
+    kandinsky_device = DEVICE_CUDA
+    if KANDINSKY_2_2_KEEP_IN_CPU_WHEN_IDLE:
+        kandinsky_device = DEVICE_CPU
+        logging.info(f"🐌 Keep in {DEVICE_CPU} when idle: Kandinsky 2.2")
+    else:
+        logging.info(f"🚀 Keep in {DEVICE_CUDA}: Kandinsky 2.2")
+    prior = KandinskyV22PriorPipeline.from_pretrained(
+        KANDINSKY_2_2_PRIOR_MODEL_ID,
+        torch_dtype=torch.float16,
+        cache_dir=SD_MODEL_CACHE,
+    ).to(kandinsky_device)
+    text2img = KandinskyV22Pipeline.from_pretrained(
+        KANDINSKY_2_2_DECODER_MODEL_ID,
+        torch_dtype=torch.float16,
+        cache_dir=SD_MODEL_CACHE,
+    ).to(kandinsky_device)
+    kandinsky_2_2 = KandinskyPipeSet_2_2(
+        prior=prior,
+        text2img=text2img,
+        inpaint=None,
+    )
+    logging.info(
+        f"✅ Loaded Kandinsky 2.2 | Duration: {round(time.time() - s, 1)} seconds"
+    )
+    return kandinsky_2_2
+
+
+def load_upscaler():
+    logging.info("🟡 Loading upscaler")
+    upscaler_pipe = AuraSR.from_pretrained(AURA_SR_MODEL_ID)
+    upscaler = Upscaler(
+        pipe=upscaler_pipe,
+    )
+    logging.info("✅ Loaded upscaler")
+    return upscaler
 
 
 def setup() -> ModelsPack:
@@ -77,198 +266,48 @@ def setup() -> ModelsPack:
         logging.info(f"✅ Logged in to HuggingFace")
 
     flux1: Flux1PipeSet = None
-
-    if FLUX1_LOAD:
-        f1_s = time.time()
-        with time_log(f"Load {FLUX1_MODEL_NAME} transformer"):
-            f1_transformer = FluxTransformer2DModel.from_single_file(
-                "https://huggingface.co/Kijai/flux-fp8/blob/main/flux1-schnell-fp8-e4m3fn.safetensors",
-                torch_dtype=FLUX1_DTYPE,
-            )
-        with time_log(f"Quantize {FLUX1_MODEL_NAME} transformer"):
-            quantize(f1_transformer, weights=qfloat8)
-
-        with time_log(f"Freeze {FLUX1_MODEL_NAME} transformer"):
-            freeze(f1_transformer)
-
-        with time_log(f"Load {FLUX1_MODEL_NAME} text_encoder_2"):
-            f1_text_encoder_2 = T5EncoderModel.from_pretrained(
-                FLUX1_REPO, subfolder="text_encoder_2", torch_dtype=FLUX1_DTYPE
-            )
-        with time_log(f"Quantize {FLUX1_MODEL_NAME} text_encoder_2"):
-            quantize(f1_text_encoder_2, weights=qfloat8)
-
-        with time_log(f"Freeze {FLUX1_MODEL_NAME} text_encoder_2"):
-            freeze(f1_text_encoder_2)
-
-        f1_pipe = FluxPipeline.from_pretrained(
-            FLUX1_REPO, transformer=None, text_encoder_2=None, torch_dtype=FLUX1_DTYPE
-        )
-        f1_pipe.transformer = f1_transformer
-        f1_pipe.text_encoder_2 = f1_text_encoder_2
-        if FLUX1_KEEP_IN_CPU_WHEN_IDLE:
-            logging.info(f"🐌 Keep in {DEVICE_CPU} when idle: {FLUX1_MODEL_NAME}")
-        else:
-            f1_pipe = f1_pipe.to(DEVICE_CUDA)
-            logging.info(f"🚀 Keep in {DEVICE_CUDA}: {FLUX1_MODEL_NAME}")
-        flux1 = Flux1PipeSet(
-            text2img=f1_pipe,
-        )
-        f1_e = time.time()
-        logging.info(
-            f"✅ Loaded {FLUX1_MODEL_NAME} | Duration: {round(f1_e - f1_s, 1)} sec."
-        )
-
     sd_pipe_sets: dict[str, SDPipeSet] = {}
-
-    for key in SD_MODELS:
-        s = time.time()
-        logging.info(f"🟡 Loading SD model: {key}")
-
-        base_model = SD_MODELS[key].get("base_model", None)
-
-        if base_model == "SDXL":
-            args = {
-                "pretrained_model_name_or_path": SD_MODELS[key]["id"],
-                "torch_dtype": SD_MODELS[key]["torch_dtype"],
-                "cache_dir": SD_MODEL_CACHE,
-                "use_safetensors": True,
-                "add_watermarker": False,
-            }
-            if "variant" in SD_MODELS[key]:
-                args["variant"] = SD_MODELS[key]["variant"]
-            text2img = StableDiffusionXLPipeline.from_pretrained(**args)
-
-            if "default_lora" in SD_MODELS[key]:
-                lora = SD_MODELS[key]["default_lora"]
-                text2img.load_lora_weights(
-                    SD_MODELS[key]["id"],
-                    weight_name=lora,
-                )
-                logging.info(f"✅ Loaded LoRA weights: {lora}")
-
-            refiner = None
-            if SD_MODELS[key].get("refiner_id") is not None:
-                refiner_args = {
-                    "pretrained_model_name_or_path": SD_MODELS[key]["refiner_id"],
-                    "torch_dtype": SD_MODELS[key]["torch_dtype"],
-                    "cache_dir": SD_MODEL_CACHE,
-                    "variant": SD_MODELS[key]["variant"],
-                    "use_safetensors": True,
-                    "add_watermarker": False,
-                }
-                if "variant" in SD_MODELS[key]:
-                    refiner_args["variant"] = SD_MODELS[key]["variant"]
-                refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained(
-                    **refiner_args
-                )
-                refiner = auto_move_to_device(SD_MODELS, key, refiner, f"{key} refiner")
-
-            text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
-            img2img = StableDiffusionXLImg2ImgPipeline(**text2img.components)
-
-            inpaint = None
-            pipe = SDPipeSet(
-                text2img=text2img,
-                img2img=img2img,
-                inpaint=inpaint,
-                refiner=refiner,
-            )
-        elif base_model == "Stable Diffusion 3":
-            args = {
-                "torch_dtype": SD_MODELS[key]["torch_dtype"],
-                "cache_dir": SD_MODEL_CACHE,
-                "safety_checker": None,
-            }
-            if "from_single_file_url" in SD_MODELS[key]:
-                args["pretrained_model_link_or_path"] = SD_MODELS[key][
-                    "from_single_file_url"
-                ]
-            else:
-                args["pretrained_model_name_or_path"] = SD_MODELS[key]["id"]
-                args["text_encoder_3"] = None
-                args["tokenizer_3"] = None
-
-            if "variant" in SD_MODELS[key]:
-                args["variant"] = SD_MODELS[key]["variant"]
-
-            text2img = None
-            if "from_single_file_url" in SD_MODELS[key]:
-                url = SD_MODELS[key]["from_single_file_url"]
-                logging.info(f"🟡 Loading SD model: {key} from single file: {url}")
-                text2img = StableDiffusion3Pipeline.from_single_file(**args)
-            else:
-                text2img = StableDiffusion3Pipeline.from_pretrained(**args)
-            text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
-            img2img = StableDiffusion3Img2ImgPipeline(**text2img.components)
-            pipe = SDPipeSet(
-                text2img=text2img,
-                img2img=img2img,
-                inpaint=None,
-                refiner=None,
-            )
-        else:
-            args = {
-                "pretrained_model_name_or_path": SD_MODELS[key]["id"],
-                "torch_dtype": SD_MODELS[key]["torch_dtype"],
-                "cache_dir": SD_MODEL_CACHE,
-                "safety_checker": None,
-            }
-            if "variant" in SD_MODELS[key]:
-                args["variant"] = SD_MODELS[key]["variant"]
-            text2img = StableDiffusionPipeline.from_pretrained(**args)
-            text2img = auto_move_to_device(SD_MODELS, key, text2img, f"{key} text2img")
-            img2img = StableDiffusionImg2ImgPipeline(**text2img.components)
-
-            pipe = SDPipeSet(
-                text2img=text2img,
-                img2img=img2img,
-                inpaint=None,
-                refiner=None,
-            )
-
-        sd_pipe_sets[key] = pipe
-        logging.info(
-            f"✅ Loaded SD model: {key} | Duration: {round(time.time() - s, 1)} seconds"
-        )
-
-    # Kandinsky 2.2
     kandinsky_2_2 = None
-    if LOAD_KANDINSKY_2_2:
-        s = time.time()
-        logging.info("🟡 Loading Kandinsky 2.2")
-        kandinsky_device = DEVICE_CUDA
-        if KANDINSKY_2_2_KEEP_IN_CPU_WHEN_IDLE:
-            kandinsky_device = DEVICE_CPU
-            logging.info(f"🐌 Keep in {DEVICE_CPU} when idle: Kandinsky 2.2")
-        else:
-            logging.info(f"🚀 Keep in {DEVICE_CUDA}: Kandinsky 2.2")
-        prior = KandinskyV22PriorPipeline.from_pretrained(
-            KANDINSKY_2_2_PRIOR_MODEL_ID,
-            torch_dtype=torch.float16,
-            cache_dir=SD_MODEL_CACHE,
-        ).to(kandinsky_device)
-        text2img = KandinskyV22Pipeline.from_pretrained(
-            KANDINSKY_2_2_DECODER_MODEL_ID,
-            torch_dtype=torch.float16,
-            cache_dir=SD_MODEL_CACHE,
-        ).to(kandinsky_device)
-        kandinsky_2_2 = KandinskyPipeSet_2_2(
-            prior=prior,
-            text2img=text2img,
-            inpaint=None,
-        )
-        logging.info(
-            f"✅ Loaded Kandinsky 2.2 | Duration: {round(time.time() - s, 1)} seconds"
-        )
+    upscaler = None
 
-    # For upscaler
-    logging.info("🟡 Loading upscaler")
-    upscaler_pipe = AuraSR.from_pretrained(AURA_SR_MODEL_ID)
-    upscaler = Upscaler(
-        pipe=upscaler_pipe,
-    )
-    logging.info("✅ Loaded upscaler")
+    with ThreadPoolExecutor() as executor:
+        futures = {}
+
+        # Flux1 model
+        if FLUX1_LOAD:
+            flux1_future = executor.submit(load_flux1_model)
+            futures[flux1_future] = "flux1"
+
+        # SD models
+        for key in SD_MODELS:
+            sd_future = executor.submit(load_sd_model, key)
+            futures[sd_future] = ("sd", key)
+
+        # Kandinsky 2.2
+        if LOAD_KANDINSKY_2_2:
+            kandinsky_future = executor.submit(load_kandinsky_2_2)
+            futures[kandinsky_future] = "kandinsky_2_2"
+
+        # Upscaler
+        upscaler_future = executor.submit(load_upscaler)
+        futures[upscaler_future] = "upscaler"
+
+        for future in as_completed(futures):
+            task = futures[future]
+            try:
+                result = future.result()
+                if task == "flux1":
+                    flux1 = result
+                elif task == "kandinsky_2_2":
+                    kandinsky_2_2 = result
+                elif task == "upscaler":
+                    upscaler = result
+                elif isinstance(task, tuple) and task[0] == "sd":
+                    key = task[1]
+                    pipe = result
+                    sd_pipe_sets[key] = pipe
+            except Exception as e:
+                logging.error(f"Error loading {task}: {e}")
 
     end = time.time()
     logging.info("//////////////////////////////////////////////////////////////////")
