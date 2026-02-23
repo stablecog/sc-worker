2cc4a3c972fdeb84737c35436e5c1d0e4f6b7ab4
yekta <ngyekta@gmail.com>
2026-02-23 04:25:04 +0300

    Remove unused code

--- shared/helpers.py
+++ shared/helpers.py
@@ -292,10 +292,7 @@ def move_pipe_to_device(pipe, model_name, device):
     if pipe is None:
         return None
     s = time.time()
-    if model_name == "upscaler":
-        pipe.upsampler = pipe.upsampler.to(device)
-    else:
-        pipe = pipe.to(device, silence_dtype_warnings=True)
+    pipe = pipe.to(device, silence_dtype_warnings=True)
     e = time.time()
     emoji = "🚀" if device == DEVICE_CUDA else "🐌"
     logging.info(
